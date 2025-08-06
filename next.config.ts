da3b3b63b886858bd87680f2ac8440fda3e711f2
v Shah <vinitkiranshah@gmail.com>
2025-08-06 12:11:52 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,5 @@ export default withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  customWorkerDir: 'public',
   disable: process.env.NODE_ENV === 'development',
 })(nextConfig);
